28112cb7e6bbf384d1b9e037c25e71c0064de8ae
Harishacharya-redhat <hacharya@redhat.com>
2025-06-06 17:57:18 +0530

    adding scinario to deploy multiple nfs cluster

---
+++ tests/nfs/test_deploy_multiple_nfs_ganesha.py
@@ -0,0 +1,35 @@
+
+
+
+from cli.ceph.ceph import Ceph
+from cli.exceptions import ConfigError
+
+
+def run(ceph_cluster, **kw):
+    """Verify create file, create soflink and lookups from nfs clients
+    Args:
+        **kw: Key/value pairs of configuration information to be used in the test.
+    """
+    config = kw.get("config")
+    nfs_nodes = ceph_cluster.get_nodes("nfs")
+    clients = ceph_cluster.get_nodes("client")
+
+    port = config.get("port", "2049")
+    version = config.get("nfs_version", "4.0")
+    no_clients = int(config.get("clients", "2"))
+
+    # If the setup doesn't have required number of clients, exit.
+    if no_clients > len(clients):
+        raise ConfigError("The test requires more clients than available")
+
+    clients = clients[:no_clients]  # Select only the required number of clients
+    fs_name = "cephfs"
+    nfs_name = "cephfs-nfs"
+    nfs_export = "/export"
+    nfs_mount = "/mnt/nfs"
+    fs = "cephfs"
+    number_of_clusters = config.get("number_of_clusters", 1)
+    
+    cluster_nodes = Ceph(clients[0]).execute_as_sudo('get nodes')
+
+    pass
